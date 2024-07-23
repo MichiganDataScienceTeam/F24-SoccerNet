96482465e5a50ad6e29d4412d5a64bcf6355e8fb
Antonio Capdevielle <138351921+antoniocap2023@users.noreply.github.com>
2024-07-23 18:21:05 +0200

    Update tracker.py

--- SoccerAnalysis/tracker.py
+++ SoccerAnalysis/tracker.py
@@ -14,6 +14,18 @@ class Tracker:
         self.player_model_id = f"{project_name}/{version_number}"
         self.tracker = sv.ByteTrack()
 
+    def add_position_to_tracks(sekf,tracks):
+        for object, object_tracks in tracks.items():
+            for frame_num, track in enumerate(object_tracks):
+                for track_id, track_info in track.items():
+                    bbox = track_info['bbox']
+                    if object == 'ball':
+                        position= get_center_of_bbox(bbox)
+                    else:
+                        position = get_foot_position(bbox)
+                    tracks[object][frame_num][track_id]['position'] = position
+    
+    
     def detect_frames(self, frames):
         detections = []
         for frame in frames:
