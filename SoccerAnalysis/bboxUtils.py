c53d3193fdc0bb38f9bb27ed33644a6658b36105
Antonio Capdevielle <138351921+antoniocap2023@users.noreply.github.com>
2024-08-24 16:20:30 -0400

    Update bboxUtils.py

--- SoccerAnalysis/bboxUtils.py
+++ SoccerAnalysis/bboxUtils.py
@@ -17,3 +17,39 @@ def get_foot_position(bbox):
 
 def get_bbox_height(bbox):
     return bbox[3] - bbox[1]
+
+def get_top_right(bbox):
+        x1, y1, x2, y2 = bbox
+        return [x2, y1]
+
+def get_top_left(bbox):
+        x1, y1, x2, y2 = bbox
+        return [x1, y1]
+
+def get_bottom_right(bbox):
+        x1, y1, x2, y2 = bbox
+        return [x2, y2]
+
+def get_bottom_left(bbox):
+        x1, y1, x2, y2 = bbox
+        return [x1, y2]
+
+def get_left_middle(bbox):
+        x1, y1, x2, y2 = bbox
+        return [x1, (y1 + y2) // 2]
+
+def get_right_middle(bbox):
+        x1, y1, x2, y2 = bbox
+        return [x2, (y1 + y2) // 2]
+
+def get_midpoint(p1, p2):
+    return [(p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2]
+
+def is_bbox_inside(outer_bbox, inner_bbox):
+    x1_out, y1_out, x2_out, y2_out = outer_bbox
+    x1_in, y1_in, x2_in, y2_in = inner_bbox
+
+    return (x1_out <= x1_in <= x2_out and
+            x1_out <= x2_in <= x2_out and
+            y1_out <= y1_in <= y2_out and
+            y1_out <= y2_in <= y2_out)
