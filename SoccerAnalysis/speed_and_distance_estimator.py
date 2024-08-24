0d47bddb45a5f831bc837c757c9a4a4361892499
Antonio Capdevielle <138351921+antoniocap2023@users.noreply.github.com>
2024-08-24 16:18:01 -0400

    Update speed_and_distance_estimator.py

--- SoccerAnalysis/speed_and_distance_estimator.py
+++ SoccerAnalysis/speed_and_distance_estimator.py
@@ -7,13 +7,13 @@ from bboxUtils import measure_distance ,get_foot_position
 class SpeedAndDistance_Estimator():
     def __init__(self):
         self.frame_window=5
-        self.frame_rate=24
+        self.frame_rate=30
     
     def add_speed_and_distance_to_tracks(self,tracks):
         total_distance= {}
 
         for object, object_tracks in tracks.items():
-            if object == "ball" or object == "referees":
+            if object != "players":
                 continue 
             number_of_frames = len(object_tracks)
             for frame_num in range(0,number_of_frames, self.frame_window):
@@ -23,8 +23,8 @@ class SpeedAndDistance_Estimator():
                     if track_id not in object_tracks[last_frame]:
                         continue
 
-                    start_position = object_tracks[frame_num][track_id]['position']
-                    end_position = object_tracks[last_frame][track_id]['position']
+                    start_position = object_tracks[frame_num][track_id]['position transformed']
+                    end_position = object_tracks[last_frame][track_id]['position transformed']
 
                     if start_position is None or end_position is None:
                         continue
@@ -33,6 +33,7 @@ class SpeedAndDistance_Estimator():
                     time_elapsed = (last_frame-frame_num)/self.frame_rate
                     speed_meteres_per_second = distance_covered/time_elapsed
                     speed_km_per_hour = speed_meteres_per_second*3.6
+                    speed_mph = speed_km_per_hour/1.609
 
                     if object not in total_distance:
                         total_distance[object]= {}
@@ -45,14 +46,14 @@ class SpeedAndDistance_Estimator():
                     for frame_num_batch in range(frame_num,last_frame):
                         if track_id not in tracks[object][frame_num_batch]:
                             continue
-                        tracks[object][frame_num_batch][track_id]['speed'] = speed_km_per_hour
+                        tracks[object][frame_num_batch][track_id]['speed'] = speed_mph
                         tracks[object][frame_num_batch][track_id]['distance'] = total_distance[object][track_id]
     
     def draw_speed_and_distance(self,frames,tracks):
         output_frames = []
         for frame_num, frame in enumerate(frames):
             for object, object_tracks in tracks.items():
-                if object == "ball" or object == "referees":
+                if object != "players":
                     continue 
                 for _, track_info in object_tracks[frame_num].items():
                     # Check if track_info is a dictionary
@@ -87,10 +88,10 @@ class SpeedAndDistance_Estimator():
                         bbox = track_info['bbox']
                         position = get_foot_position(bbox)
                         position = list(position)
-                        position[1]+=40
+                        position[1]+=100
 
                         position = tuple(map(int,position))
-                        cv2.putText(frame, f"{speed:.2f} km/h",position,cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,0),2)
+                        cv2.putText(frame, f"{speed:.2f} mph",position,cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,0),2)
                         cv2.putText(frame, f"{distance:.2f} m",(position[0],position[1]+20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,0),2)
             output_frames.append(frame)
         
