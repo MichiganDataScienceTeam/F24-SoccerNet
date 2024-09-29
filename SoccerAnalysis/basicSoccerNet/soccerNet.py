14e7dad3362054fafc5b976cc90c38b1cc410072
Brandon Nguyen <benguyen@umich.edu>
2024-09-29 13:21:24 -0400

    added name and fav animal

--- SoccerAnalysis/basicSoccerNet/soccerNet.py
+++ SoccerAnalysis/basicSoccerNet/soccerNet.py
@@ -72,3 +72,6 @@ for index, detections in enumerate(all_detections):
 cap.release()
 out.release()
 print(f"Processed video saved to {TARGET_VIDEO_PATH}")
+
+
+# Brandon's favorite animal is a tiger
