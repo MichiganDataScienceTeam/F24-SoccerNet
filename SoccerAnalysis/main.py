187d0b6b1979bf1d4f06acfe09a1b342d0ed932b
Antonio Capdevielle <138351921+antoniocap2023@users.noreply.github.com>
2024-10-21 19:18:46 -0400

    Update main.py
    packaged main.py

--- SoccerAnalysis/main.py
+++ SoccerAnalysis/main.py
@@ -3,16 +3,18 @@ import os
 import cv2
 import pickle
 from roboflow import Roboflow
-from assign_team import TeamAssigner
+from assign_teams import TeamAssigner
 from tracker import Tracker
 from line import WhiteLineDetector
 from interpolator import Interpolator
 from camera_movement import CameraMovementEstimator
 from speed_and_distance_estimator import SpeedAndDistance_Estimator
-from intersection_finder import SuperAlgorithm
+from algorithmic_intersection_finder import SuperAlgorithm
 from transformer import ViewTransformer
 import videoUtils
 
+
+'''
 SOURCE_VIDEO_PATH = "20SecGoodVid.mov"
 TARGET_VIDEO_PATH = "interpolation_test4.mp4"
 PlAYER_API_KEY = ""
@@ -22,90 +24,110 @@ VERSION_NUMBER = 1
 BOXES_API_KEY = ""
 BOXES_PROJECT_NAME = "football0detections"
 STUB_FRAMES_PATH = "frames_stub.pkl"
+'''
 
+def process_video(source_video_path, target_video_path, player_api_key, player_stub_path,
+                  player_project_name, version_number, boxes_api_key, boxes_project_name, stub_frames_path,
+                  field_length=105, field_width=68):
+    """
+    Process a video, track player movement, and return annotated frames.
 
-# Initialize the Plyer Roboflow model and Tracker
-player_rf = Roboflow(api_key=PlAYER_API_KEY)
-boxes_rf = Roboflow(api_key=BOXES_API_KEY)
+    Parameters:
+    source_video_path (str): Path to the source video.
+    target_video_path (str): Path to save the processed video (optional).
+    player_api_key (str): API key for player detection Roboflow model.
+    player_stub_path (str): Path to player stub file.
+    player_project_name (str): Roboflow project name for player detection.
+    version_number (int): Version number for the models.
+    boxes_api_key (str): API key for boxes detection Roboflow model.
+    boxes_project_name (str): Roboflow project name for box detection.
+    stub_frames_path (str): Path to frames stub file.
+    field_length (int): Length of the soccer field in meters.
+    field_width (int): Width of the soccer field in meters.
 
-player_project = player_rf.workspace().project(PLAYER_PROJECT_NAME)
-boxes_project = boxes_rf.workspace().project(BOXES_PROJECT_NAME)
+    Returns:
+    frames (list): Annotated video frames with object tracks.
+    """
 
-player_model = player_project.version(VERSION_NUMBER).model
-boxes_model = boxes_project.version(VERSION_NUMBER).model
+    # Initialize the Plyer Roboflow model and Tracker
+    player_rf = Roboflow(api_key=player_api_key)
+    boxes_rf = Roboflow(api_key=boxes_api_key)
 
-print("Roboflow models initialized")
+    player_project = player_rf.workspace().project(player_project_name)
+    boxes_project = boxes_rf.workspace().project(boxes_project_name)
 
-tracker = Tracker(PlAYER_API_KEY, PLAYER_PROJECT_NAME, VERSION_NUMBER, BOXES_API_KEY, BOXES_PROJECT_NAME, VERSION_NUMBER)
-print("Tracker Initialized")
+    player_model = player_project.version(version_number).model
+    boxes_model = boxes_project.version(version_number).model
 
+    print("Roboflow models initialized")
 
-frames = videoUtils.read_video(SOURCE_VIDEO_PATH)
+    tracker = Tracker(player_api_key, player_project_name, version_number, boxes_api_key, boxes_project_name, version_number)
+    print("Tracker Initialized")
+
+    frames = videoUtils.read_video(source_video_path)
     
-    
-tracks = tracker.get_object_tracks(frames, read_from_stub=True, stub_path="stub_path_mid.pkl")
+    tracks = tracker.get_object_tracks(frames, read_from_stub=True, stub_path=player_stub_path)
 
-tracker.add_position_to_tracks(tracks)
-print("Adding Positions To Tracks")
-    
-interpolator = Interpolator()
-tracks = interpolator.interpolate_missing_bboxes(tracks)
-    
-print("Getting Field Intersection Points")
-white_line_detector = WhiteLineDetector()
-tracks = white_line_detector.get_intersections(frames, tracks, 'intersections_mid.pkl', 'intersections_mid.pkl')
+    tracker.add_position_to_tracks(tracks)
+    print("Adding Positions To Tracks")
 
-print("Finalizing Stationary Points")
-super_algorithm = SuperAlgorithm()
-tracks = super_algorithm.find_middle_intersections(tracks)
-tracks = super_algorithm.find_outer_intersections(tracks)
-tracks = super_algorithm.find_right_field_intersections(tracks)
-tracks = super_algorithm.find_left_field_intersections(tracks)
-#tracks = super_algorithms.find_left_field_intersections(tracks)
+    interpolator = Interpolator()
+    tracks = interpolator.interpolate_missing_bboxes(tracks)
 
-print("Initializing Camera Movement")
-camera_movement_estimator = CameraMovementEstimator(frames[0])
-camera_movement_per_frame = camera_movement_estimator.get_camera_movement(tracks, frames, read_from_stub=True, stub_path='camera_movement_stub.pkl')
+    print("Getting Field Intersection Points")
+    white_line_detector = WhiteLineDetector()
+    tracks = white_line_detector.get_intersections(frames, tracks, 'intersections_mid.pkl', 'intersections_mid.pkl')
 
-print("Adding Camera Movement To Tracks")
-camera_movement_estimator.add_adjust_positions_to_tracks(tracks,camera_movement_per_frame)
+    print("Finalizing Stationary Points")
+    super_algorithm = SuperAlgorithm()
+    tracks = super_algorithm.find_middle_intersections(tracks)
+    tracks = super_algorithm.find_outer_intersections(tracks)
+    tracks = super_algorithm.find_right_field_intersections(tracks)
+    tracks = super_algorithm.find_left_field_intersections(tracks)
 
+    print("Initializing Camera Movement")
+    camera_movement_estimator = CameraMovementEstimator(frames[0])
+    camera_movement_per_frame = camera_movement_estimator.get_camera_movement(tracks, frames, read_from_stub=True, stub_path=stub_frames_path)
 
-print("Computing Homography")
-view_transformer = ViewTransformer(camera_movement_per_frame, field_length=105, field_width=68)
-view_transformer.apply_homography(frames, tracks)
+    print("Adding Camera Movement To Tracks")
+    camera_movement_estimator.add_adjust_positions_to_tracks(tracks, camera_movement_per_frame)
 
-print("Adding Transformed Position To Tracks")
-view_transformer.add_transformed_position_to_tracks(tracks)
+    print("Computing Homography")
+    view_transformer = ViewTransformer(camera_movement_per_frame, field_length=field_length, field_width=field_width)
+    view_transformer.apply_homography(frames, tracks)
 
+    print("Adding Transformed Position To Tracks")
+    view_transformer.add_transformed_position_to_tracks(tracks)
 
-speed_and_distance_estimator = SpeedAndDistance_Estimator()
-speed_and_distance_estimator.add_speed_and_distance_to_tracks(tracks)
+    speed_and_distance_estimator = SpeedAndDistance_Estimator()
+    speed_and_distance_estimator.add_speed_and_distance_to_tracks(tracks)
 
-team_assigner = TeamAssigner()
-team_assigner.assign_team_color(frames[0], tracks['players'][0])
-for frame_num, player_track in enumerate(tracks['players']):
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(frames[0], tracks['players'][0])
+    for frame_num, player_track in enumerate(tracks['players']):
         for track_id, track in player_track.items():
             team = team_assigner.get_player_team(frames[frame_num], track['bbox'], track_id)
             tracks['players'][frame_num][track_id]['team'] = team
             tracks['players'][frame_num][track_id]['team_color'] = team_assigner.team_colors[team]
 
+    print("Drawing Speed and Distances")
+    frames = speed_and_distance_estimator.draw_speed_and_distance(frames, tracks)
 
-print("Drawing Speed and Distances")
-frames = speed_and_distance_estimator.draw_speed_and_distance(frames, tracks)
+    print("Drawing Camera Movement")
+    frames = camera_movement_estimator.draw_camera_movement(frames, camera_movement_per_frame)
 
-print("Drawing Camera Movement")
-frames = camera_movement_estimator.draw_camera_movement(frames,camera_movement_per_frame)
+    print("Drawing Transformed Positions")
+    frames = view_transformer.draw_transformed_position(frames, tracks)
 
+    print("Drawing Field Intersection Points")
+    frames = white_line_detector.draw_frame_intersections(frames, tracks)
 
-print("Drawing Transformed Positions")
-frames = view_transformer.draw_transformed_position(frames, tracks)
+    print("Drawing Annotations")
+    annotated_frames = tracker.draw_annotations(frames, tracks)
 
-print("Drawing Field Intersection Points")
-frames = white_line_detector.draw_frame_intersections(frames, tracks)
+    # Optionally save video to the target path
+    if target_video_path:
+        videoUtils.save_video(annotated_frames, target_video_path)
+        print("Video saved successfully")
 
-print("Drawing Annotations")
-annotated_frames = tracker.draw_annotations(frames, tracks)
-
-videoUtils.save_video(annotated_frames, TARGET_VIDEO_PATH) #SWITCH TO FINAL_ANNOTATED_FRAMES
-print("Video saved successfully")
+    return annotated_frames
